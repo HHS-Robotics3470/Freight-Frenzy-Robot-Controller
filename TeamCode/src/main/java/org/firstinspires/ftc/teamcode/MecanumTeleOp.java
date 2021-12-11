afdbb5d50d279e2acfc02fd04dbb6b6deb4f81e5
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2021-12-11 11:05:19 -0800

    oops

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -17,6 +17,8 @@ public class MecanumTeleOp extends LinearOpMode {
         ////////////before driver presses play////////////
         //Variables
         int outputFlipperState = 0; //0-3, 0=retracked, 1=extended flat, 2=extended up, 3=extended down
+        boolean inputOpen = true;
+        boolean outputOpen = false;
         int cascadeCount = 0;
         double[] leftStickPolar;
         //button locks
@@ -48,14 +50,14 @@ public class MecanumTeleOp extends LinearOpMode {
             triggers and bumpers: unassigned
 
             ABXY: special functions
-                A: grab item w/ input servo (open/close input servo)
-                B: flip intake and drop off in output basket (retract if necessary)
+                A: grab item w/ input servo (open/close input servo) //TODO: readjust
+                B: flip intake and drop off in output basket (retract if necessary) //TODO: readjust flipper
                 X: drop item off (open/close output servo)
                 Y: if output flipper is extended, retract, otherwise extend to flat
 
             D-Pad:
-                Up: if output flipper is extended flat, go to extended up; if output flipper is extended down, go to extended flat; if output flipper is retracted, extend flat
-                Down: if output flipper is extended flat, go to extended down; if output flipper is extended up, go to extended flat; if output flipper is retracted, extend flat
+                Up: if output flipper is extended flat, go to extended up; if output flipper is extended down, go to extended flat; if output flipper is extended up, extend down
+                Down: if output flipper is extended flat, go to extended down; if output flipper is extended up, go to extended flat; if output flipper is extended down, extend up
                 Left: extend cascade kit
                 Right: retract cascade kit
 
@@ -78,7 +80,17 @@ public class MecanumTeleOp extends LinearOpMode {
             yC = gamepad1.y;
             //A
             if (aC&&!aP) {
-                robot.frontInputServo.setPosition((-1 * (robot.frontInputServo.getPosition() - 0.285) + 0.285) + 0.25); //toggle between 0.25 and .72
+                //robot.frontInputServo.setPosition((-1 * (robot.frontInputServo.getPosition() - 0.285) + 0.285) + 0.25); //toggle between 0.25 and .72
+                if (inputOpen) { // if open
+                    robot.frontInputServo.setPosition(0.72); //close
+                    sleep(300);
+                    robot.frontInputFlipperServo.setPosition(0.2); //raise
+                    inputOpen = false;
+                } else { //else
+                    robot.frontInputServo.setPosition(0.3); //open
+                    robot.frontInputFlipperServo.setPosition(0); //lower
+                    inputOpen = true;
+                }
             }
             //B
             cascadeCount = robot.cascadeLiftMotor.getCurrentPosition();
@@ -89,30 +101,37 @@ public class MecanumTeleOp extends LinearOpMode {
                 robot.cascadeFlipperServo.setPosition(0.125); //fold back
                 robot.runMotorToPosition(robot.cascadeLiftMotor, 50, 1);//retract cascade
 
-                robot.frontInputFlipperServo.setPosition(1);//TODO rotate input flipper to drop off position
-                sleep(200);
+                robot.frontInputFlipperServo.setPosition(0.7);//TODO rotate input flipper to drop off position
+                sleep(700);
 
                 robot.frontInputServo.setPosition(0.55); //open input
-                sleep(100);
+                sleep(300);
 
-                robot.frontInputFlipperServo.setPosition(0.75);//TODO rotate input flipper to collection position
+                robot.frontInputFlipperServo.setPosition(0);//TODO rotate input flipper to collection position
                 robot.frontInputServo.setPosition(0.25); //fully open input
+                inputOpen = true;
                 robot.cascadeOutputServo.setPosition(1); //close output
+                outputOpen = false;
                 robot.runMotorToPosition(robot.cascadeLiftMotor, cascadeCount, 1); //extend cascade back to previous position
                 robot.cascadeFlipperServo.setPosition(outputFlipperPosition); //move output back to level it was at previously
-                sleep(200);
-
+                //sleep(200);
             }
             //X
             if (xC&&!xP) {
-                robot.cascadeOutputServo.setPosition((-1 * (robot.cascadeOutputServo.getPosition() - 0.125) + 0.125) + 0.75); //toggle between 0.75 and 1
+                if (outputOpen) {
+                    robot.cascadeOutputServo.setPosition(1);
+                } else {
+                    robot.cascadeOutputServo.setPosition(0.75);
+                }
             }
             //Y
             if (yC&&!yP) {
-                if (robot.cascadeFlipperServo.getPosition() > 0.125 ){
+                if (outputFlipperState != 0){
                     robot.cascadeFlipperServo.setPosition(0.125);
+                    outputFlipperState=0;
                 } else {
                     robot.cascadeFlipperServo.setPosition(0.8);
+                    outputFlipperState=1;
                 }
             }
             aP=aC;
@@ -124,11 +143,29 @@ public class MecanumTeleOp extends LinearOpMode {
             downC = gamepad1.dpad_down;
             //UP
             if (upC&&!upP) {
-
+                if(outputFlipperState == 1) { //extended flat
+                    robot.cascadeFlipperServo.setPosition(0.6); //up
+                    outputFlipperState=2;
+                } else if (outputFlipperState == 3) { //extended down
+                    robot.cascadeFlipperServo.setPosition(0.8); //flat
+                    outputFlipperState=1;
+                } else if (outputFlipperState == 2) { //extended up
+                    robot.cascadeFlipperServo.setPosition(0.95); //down
+                    outputFlipperState=3;
+                }
             }
             //DOWN
             if (downC&&!downP) {
-
+                if(outputFlipperState == 1) { //extended flat
+                    robot.cascadeFlipperServo.setPosition(0.95); //down
+                    outputFlipperState=3;
+                } else if (outputFlipperState == 2) { //extended up
+                    robot.cascadeFlipperServo.setPosition(0.8); //flat
+                    outputFlipperState=1;
+                } else if (outputFlipperState == 3) { //extended down
+                    robot.cascadeFlipperServo.setPosition(0.6); //up
+                    outputFlipperState=2;
+                }
             }
             upP = upC;
             downP = downC;

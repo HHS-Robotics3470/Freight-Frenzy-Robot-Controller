0284949a427963d7d31b756435a6299f4593d954
Adeel <theadeelahmad07@gmail.com>
2022-01-17 22:01:36 -0800

    Adeel's auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tableParkingAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tableParkingAutonomous.java
@@ -24,13 +24,21 @@ public class warehouseParkingAuto extends LinearOpMode{
     {
         ////////////before driver presses play////////////
         //Variables
+        double init_move = 0.1524;
+        double angle_one = 4.71239;
+        double angle_two = -0.785398;
+        double second_move = 0.1524;
+        double angle_three = -0.785398;
+        double angle_four = 2.35619;
+        double third_move = 1;
+
 
 
         /* Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
          */
         robot.init(hardwareMap);
-
+        robot.initVuforiaAndTfod(hardwareMap);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -39,6 +47,19 @@ public class warehouseParkingAuto extends LinearOpMode{
         ////////////after driver presses play////////////
 
         //autonomous routine goes here step by step
+
+        //Orient robot so that turn does not hit the field wall
+        robot.strafeToDistance(1, angle_one, init_move);
+        robot.rotateByAngle(angle_two, 1);
+        robot.strafeToDistance(1, angle_three, second_move);
+        robot.turntableMotor.setPower(1);
+        sleep(500);
+        robot.turntableMotor.setPower(0);
+        robot.strafeToDistance(1, angle_three, -second_move);
+        robot.rotateByAngle(angle_four, 1);
+        robot.strafeToDistance(1, angle_one, third_move);
+
+
 
 
         ////////////after driver presses stop////////////
